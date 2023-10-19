4596d61b74daa6862a753360c77d592649d024c4
Fersy van der Zee <62746506+Fersyvanderzee@users.noreply.github.com>
2023-10-19 17:30:25 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,3 @@
-from PIL import ImageDraw
-
-
 def linear_map(value, from_low, from_high, to_low, to_high):
     """
     Linearly maps a value from one range to another.
@@ -57,34 +54,3 @@ def convert_hex_to_rgb(rgb_color: tuple):
     return hex_color
 
 
-def create_gradient(image, img_width: int, img_height: int, start_color: tuple, end_color: tuple):
-    """
-    Creates the gradient on the background of the image.
-
-        Args:
-            image: the image on which to draw.
-            img_width (int): the width of the image to draw on.
-            img_heigth (int): the height of the image to draw on.
-            start_color (tuple): the starting color (left color).
-            end_color (tuple): the ending color (right color).
-    """
-
-    if not isinstance(img_width, int):
-        raise ValueError("img_width is not an int")
-    
-    if not isinstance(start_color, tuple):
-        raise ValueError("start_color is not a tuple")
-    
-    if not isinstance(end_color, tuple):
-        raise ValueError("end_color is not a tuple")
-
-    for x in range(img_width):
-        r = int(start_color[0] * (1 - x / img_width) + end_color[0] * (x / img_width))
-        g = int(start_color[1] * (1 - x / img_width) + end_color[1] * (x / img_width))
-        b = int(start_color[2] * (1 - x / img_width) + end_color[2] * (x / img_width))
-
-        color = (r, g, b)
-
-        draw = ImageDraw.Draw(image)
-
-        draw.line([(x, 0), (x, img_height)], fill=color)
